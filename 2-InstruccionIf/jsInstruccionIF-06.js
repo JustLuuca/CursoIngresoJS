fb215a60618ae45f8225decd0e788dfea83ccee3
JustLuuca <lucagarcete846@gmail.com>
2023-02-07 12:01:42 -0300

    if v1

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,7 +1,28 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	let mensaje;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	if(edad > 17)
+	{
+		mensaje = "Es mayor de edad";
+	}else {
+		
+		if (edad >=13 && edad <=17)
+		{
+			mensaje = "Es adolescente"
+	}else {
+			mensaje = "Es menor de edad";
+
+	}
+	
+	}
+	alert(mensaje);
+
+
+	
 
 
 
