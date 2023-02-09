354a0790551f34ac0628366909827d2b93398b07
JustLuuca <lucagarcete846@gmail.com>
2023-02-09 11:49:24 -0300

    switch v1

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -1,8 +1,30 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
+	
+	mesDelAño = document.getElementById("txtIdMes").value;
+
+	switch(mesDelAño){
+		case "Enero":
+			mensaje = "que comiences bien el año!!!"
+			break;
+		case "Marzo":
+			mensaje = "a clases!!!"
+			break;
+		case "Julio":
+			mensaje = "se vienen las vacaciones!!!"
+			break;
+		case "Diciembre":
+			mensaje = "felices fiestas!!!";
+			break;
+				
+			
+			
+	}
+		alert(mensaje);
+
+	
 
 
 
