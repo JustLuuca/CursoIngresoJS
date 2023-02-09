82688658ed0a072ab12420d6591eaf240e1a93d2
JustLuuca <lucagarcete846@gmail.com>
2023-02-09 12:11:25 -0300

    switch v1

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,9 +1,22 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
+
+	mesDelAño = document.getElementById("txtIdMes").value;
 	
+	switch(mesDelAño){
+		case "Febrero":
+			mensaje = "Este mes NO tiene mas de 29 dias"
+			break;
+
+			default:
+			mensaje = "Este mes tiene 30 dias o mas"
+			break;
+
+			
+	}
+	  alert(mensaje);
 	
 
 
