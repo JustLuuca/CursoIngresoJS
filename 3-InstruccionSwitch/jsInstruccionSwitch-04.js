52705eb010a96ce9619670f51f5fbb7387757d30
JustLuuca <lucagarcete846@gmail.com>
2023-02-10 20:06:35 -0300

    switch v1

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,8 +1,42 @@
+/*septiembre, con abril, junio y noviembre 30
+/*febrero 28     
+/*enero marzo mayo octubre julio agosto diciembre 31*/
+
 function mostrar()
+
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mesDelAño;
+	let mensaje;
+
+	mesDelAño = document.getElementById("txtIdMes").value;
+
+	switch(mesDelAño){
+		
+		case "Febrero":
+			mensaje = "Este mes tiene 28 dias"
+			break;
+
+		case "Septiembre":
+		case "Abril":
+		case "Junio":
+		case "Noviembre":
+			mensaje = "Este mes tiene 30 dias"
+			break;
+			
+		default:
+			mensaje = "Este mes tiene 31 dias"
+			break;
+
+
+	}
+
+		alert(mensaje);
+
+
+
+
+
+	
 	
 	
 
