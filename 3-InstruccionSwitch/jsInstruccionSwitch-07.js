02eafcb26710c74ed6cf58a322e8ccfbeb9ce841
JustLuuca <lucagarcete846@gmail.com>
2023-02-15 00:19:40 -0300

    switch v1

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,37 @@
+/*bariloche oeste
+cataratas norte
+ushuaia sur
+mardel este*/
+
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+	let mensaje;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+
+	switch(destino){
+		case "Bariloche":
+			mensaje = "Se encuentra en el oeste"
+			break;
+
+		case "Cataratas":
+			mensaje = "Se encuentra en el norte"
+			break;
+
+		case "Mar del plata":
+			mensaje = "Se encuentra en el este"
+			break;
+
+		case "Ushuaia":
+			mensaje = "Se encuentra en el sur"
+			break;
+			
+	}
+		alert(mensaje);
+
+
+	
 
 }//FIN DE LA FUNCIÓN
