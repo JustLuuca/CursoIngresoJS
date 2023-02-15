b9c6dc8fa63afc84144443c3ca53afa4171597b2
JustLuuca <lucagarcete846@gmail.com>
2023-02-15 00:24:00 -0300

    switch v1

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,29 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let destino;
+	let mensaje;
+
+	destino = document.getElementById("txtIdDestino").value;
+
+
+	switch(destino){
+		case "Bariloche":
+			mensaje = "En este destino hace frio"
+			break;
+
+		case "Cataratas":
+			mensaje = "En este destino hace calor"
+			break;
+
+		case "Mar del plata":
+			mensaje = "En este destino hace calor"
+			break;
+
+		case "Ushuaia":
+			mensaje = "En este destino hace frio"
+			break;
+			
+	}
+		alert(mensaje);
 
 }//FIN DE LA FUNCIÓN
