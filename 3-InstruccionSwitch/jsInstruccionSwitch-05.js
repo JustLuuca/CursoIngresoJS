8e2c78cf55e8807fce53e843e0019cb635938dbf
JustLuuca <lucagarcete846@gmail.com>
2023-02-14 23:46:12 -0300

    switch v1

--- 3-InstruccionSwitch/jsInstruccionSwitch-05.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-05.js
@@ -1,11 +1,25 @@
+/*if (hora)
+switch(hora)
+else(no existe la hora)*/
 function mostrar()
 {
-	//tomo la hora
-	var horaDelDia =txtIdHora.value;
-	alert(horaDelDia);
-	
+	let hora;
+	let mensaje;
+
+	hora = parseInt(document.getElementById("txtIdHora").value);
 	
 
+	switch(hora){
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+			mensaje = "Es de mañana"
+			break;
+				
+	}
+	  	alert(mensaje);
 
 
 }//FIN DE LA FUNCIÓN
