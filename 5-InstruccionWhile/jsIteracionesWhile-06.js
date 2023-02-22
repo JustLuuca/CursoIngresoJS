84b9a6b0399377239804210015520e12436d51b5
JustLuuca <lucagarcete846@gmail.com>
2023-02-22 01:19:23 -0300

    while v1

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,13 +1,22 @@
 function mostrar()
 {
-	var contador;
-	var acumulador;
-	var numeroIngresado;
+	
+	let numero;
+	let acumulador = 0;
+	let suma;
+	let promedio;
+	let i = 0;
 
-	contador=0;
-	acumulador=0;
-	
-	
-	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+	while( i < 5){
+		numero = parseInt(prompt("Ingrese su numero: "));
+
+		acumulador = acumulador + numero;
+
+		i++;
+	}
+
+	promedio = acumulador / 5;
+
+	document.getElementById("txtIdSuma").value = acumulador;
+	document.getElementById("txtIdPromedio").value = promedio;
 }//FIN DE LA FUNCIÓN
