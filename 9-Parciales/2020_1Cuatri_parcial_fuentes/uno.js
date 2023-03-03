7a0b6d25cb7195f9fd2aaa4f5fa12a4589d8904d
JustLuuca <lucagarcete846@gmail.com>
2023-03-03 17:35:56 -0300

    ejercicio pre parcial

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,89 +1,35 @@
 
 function mostrar()
 {
-	let tipoProducto;
-	let precioProducto;
-	let unidadesProducto;
-	let marcaProducto;
-	let fabricanteProducto;
-	let contadorAlcohol;
-	let contadorBarbijo;
-	let contadorJabon;
-	let acumuladorAlcohol;
-	let acumuladorBarbijo;
-	let acumuladorJabon;
-	let unidadesCantidadMasBaratoAlcohol;
-	let fabricanteMasBaratoAlcochol;
-	let precioMasBaratoAlcohol;
-	let flagAlcohol;
-	let promedioCompra;
-	let mayorTipo; 
+let nombreTitular;
+let destino;
+let temporada;
+let cantidadDias;
+let importe;
+let alturaPasajero;
+let pesoPasajero;
+let sexoPasajero;
+let equipaje;
+let metodoDePago;
 
-	flagAlcohol = 0;
-	
-
-	contadorAlcohol = 0;
-	contadorBarbijo = 0;
-	contadorJabon = 0;
-	acumuladorAlcohol = 0;
-	acumuladorBarbijo = 0;
-	acumuladorJabon = 0;
-
-	for (let i = 0; i < 5; i++) {
-		tipoProducto = prompt("Ingrese el nombre del producto: barbijo, jabon o alcohol");
-		while (tipoProducto != "barbijo" && tipoProducto != "jabon" && tipoProducto != "alcohol") {
-			tipoProducto = prompt("Error- Ingrese el nombre del producto: barbijo, jabon o alcohol");
-		}
-
-		precioProducto = parseFloat(prompt("Ingrese el precio del producto unitario: "));
-		while (isNaN(precioProducto) || precioProducto < 100 || precioProducto > 300) {
-			precioProducto = parseFloat(prompt("Error- Ingrese el precio del producto: "));
-		}
-
-		unidadesProducto = parseInt(prompt("ingrese la cantidad de unidades del producto: "));
-		while (isNaN(unidadesProducto) || unidadesProducto < 1 || unidadesProducto > 1000) {
-			unidadesProducto = parseInt(prompt("Error- ingrese la cantidad de unidades del producto: "));
-		}
-
-		marcaProducto = prompt("Ingrse la marca del producto: ");
-		fabricanteProducto = prompt("ingrese el fabricante del producto: ");
-
-		switch (tipoProducto) {
-			case "alcohol":
-				if (flagAlcohol == 0 || precioProducto < precioMasBaratoAlcohol) {
-					fabricanteMasBaratoAlcochol = fabricanteProducto;
-					unidadesCantidadMasBaratoAlcohol = unidadesProducto;
-					precioMasBaratoAlcohol = precioProducto;
-					flagAlcohol = 1;
-				} 
-				contadorAlcohol = contadorAlcohol + 1;
-				acumuladorAlcohol = unidadesProducto + acumuladorAlcohol;
-				break;
-			case "barbijo":
-				contadorBarbijo = contadorBarbijo + 1;
-				acumuladorBarbijo = acumuladorBarbijo + unidadesProducto;
-				break;
-			case "jabon":
-				contadorJabon = contadorJabon + 1 ;
-				acumuladorJabon = acumuladorJabon + unidadesProducto;
-				break;
-		}
-
-	}//fin del for 
-
-	if(acumuladorAlcohol > acumuladorJabon){
-		mayorTipo = "el alcohol"; 
-		promedioCompra = acumuladorAlcohol / contadorAlcohol;
-	}else if(acumuladorJabon > acumuladorBarbijo){
-		promedioCompra = acumuladorJabon / contadorJabon;
-		mayorTipo = " el jabon"; 
-	}else{
-		promedioCompra = acumuladorBarbijo / contadorBarbijo;
-		mayorTipo = " el barbijo"; 
-	}
-
-	console.log("Cantidad de unidades del mas barato de los alcohol: " + unidadesCantidadMasBaratoAlcohol + ". Su fabricante es: " + fabricanteMasBaratoAlcochol);
-	console.log("Unidades totales de jabon: " + acumuladorJabon);
-	console.log("El tipo con mas unidades es: " + mayorTipo);
-	console.log("El promedio de compra del tipo con mas unidades es: " + promedioCompra);
+nombreTitular = prompt("Ingrese su nombre")
+{
+    while(!isNaN(nombreTitular)){
+        nombreTitular = prompt("Error. Ingrese su nombre")
+    }
+    destino = prompt("Escoja su destino: 'Puerto Madryn', 'Villa Gessel', 'Córdoba'")
+    {
+        while(!isNaN(destino != 'Puerto Madryn' && destino != 'Villa Gessel' && destino != 'Córdoba'));
+        {
+            destino = prompt("Error. Ingrese un destino válido: 'Puerto Madryn', 'Villa Gessel', 'Córdoba'")
+        }
+    }
+    temporada = prompt("Viaja en temporada: 'Alta', 'Baja'")
+        while(!isNaN(temporada != 'Alta' && temporada != 'Baja'));
+        {
+            temporada = prompt("Error. Ingrese una temporada valida")
+        }
+    
+    
+}
 }
