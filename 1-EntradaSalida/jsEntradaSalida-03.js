a13261085ee03a2a86dcef5fc11400419c725f04
JustLuuca <lucagarcete846@gmail.com>
2023-06-13 23:38:17 -0300

    2da oportunidad

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,14 +3,15 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	let nombreIngresado;
-	
+	// declaracion de variables
 	let mensaje;
-	
+	let nombreIngresado;
+
+	// valor de las variables
 	nombreIngresado = document.getElementById("txtIdNombre").value;
-	
-	mensaje = "Usted ingresó el nombre:" + nombreIngresado;
-	
+
+	mensaje = "Usted ingresó el nombre: " + nombreIngresado;
+
 	alert(mensaje);
 
 }
